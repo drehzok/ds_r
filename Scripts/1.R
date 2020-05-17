65f150d2f74c3a67117301ea87763fdacad41a15
drehzok <sungbae.chun@nu.edu.kz>
2020-05-17 10:07:34 +0600

    R markdown added

--- Scripts/1.R
+++ Scripts/1.R
@@ -1,1 +1,17 @@
 print("This is the first program")
+
+print("<- is the same as = in python3")
+
+x<- 20
+print(x)
+
+y <- 1:50
+print(y)
+
+
+v = c(1:20)
+#creating a vector
+v
+
+class(v)
+class(c(1,2))
